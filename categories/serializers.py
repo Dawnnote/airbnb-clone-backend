52003b412082d6f8667ed54be9998522f8f3637a
Dawnnote <lshoon7454@gmail.com>
2023-03-09 00:24:14 +0900

    Update_Serializers

--- categories/serializers.py
+++ categories/serializers.py
@@ -5,4 +5,7 @@ from .models import Category
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        exclude = ("created_at",)
+        fields = (
+            "name",
+            "kind",
+        )
