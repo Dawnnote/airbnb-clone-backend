2d49d16213dbf3ce45f32906aa9bc159f3f3fc70
Dawnnote <lshoon7454@gmail.com>
2023-03-14 16:50:41 +0900

    is_owner

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -37,6 +37,7 @@ class RoomDetailSerializer(serializers.ModelSerializer):
 class RoomListSerializer(serializers.ModelSerializer):
 
     rating = serializers.SerializerMethodField()
+    is_owner = serializers.SerializerMethodField()
 
     class Meta:
         model = Room
@@ -47,7 +48,12 @@ class RoomListSerializer(serializers.ModelSerializer):
             "city", 
             "price",
             "rating",
+            "is_owner",
         )
 
     def get_rating(self, room):
         return room.rating()
+    
+    def get_is_owner(self, room):
+        request = self.context['request']
+        return room.owner == request.user
