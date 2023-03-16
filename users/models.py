c628d06a7dfec6038401cbbe8e2d9a634ac2a826
Dawnnote <lshoon7454@gmail.com>
2023-03-16 15:07:43 +0900

    user_profile_change

--- users/models.py
+++ users/models.py
@@ -16,7 +16,7 @@ class User(AbstractUser):
 
     first_name = models.CharField(max_length=150, editable=False)
     last_name = models.CharField(max_length=150, editable=False)
-    avator = models.ImageField(blank=True)
+    avator = models.URLField(blank=True)
     name = models.CharField(max_length=150, default="")
     is_host = models.BooleanField(default=False)
 
--- users/serializers.py
+++ users/serializers.py
@@ -5,3 +5,19 @@ class TinyUserSerializer(ModelSerializer):
     class Meta:
         model = User
         fields = ("name", "avator", "username")
+
+
+class PrivateUserSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        exclude = (
+            "password",
+            "is_superuser",
+            "id",
+            "is_staff",
+            "is_active",
+            'first_name',
+            "last_name",
+            "groups",
+            "user_permissions"
+        )
